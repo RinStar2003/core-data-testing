9d70318c53c79215181d1735e9d23173e2735473
RinStar2003 <mituha133@gmail.com>
2022-06-02 20:45:10 +0300

    Initial Commit

---
+++ CoreDataTest/CoreDataTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataTestApp.swift
+//  CoreDataTest
+//
+//  Created by мас on 02.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
