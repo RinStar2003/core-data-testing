dc2e94f2c908f341f0d45c4371201a781b9b96a4
RinStar2003 <mituha133@gmail.com>
2022-06-21 12:51:14 +0300

    - Just a commit

---
+++ CoreDataTest/Views/AddFoodView.swift
@@ -0,0 +1,45 @@
+//
+//  AddFoodView.swift
+//  CoreDataTest
+//
+//  Created by мас on 03.06.2022.
+//
+
+import SwiftUI
+
+struct AddFoodView: View {
+    @Environment (\.managedObjectContext) var managedObjectContext
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var calories: Double = 0
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Fool name", text: $name)
+                
+                VStack {
+                    Text("Calories: \(Int(calories))")
+                    Slider(value: $calories, in: 0...2000, step: 10)
+                }
+                .padding()
+                
+                HStack {
+                    Spacer()
+                    Button("Submit") {
+                        DataController().addFood(name: name, calories: calories, context: managedObjectContext)
+                        dismiss()
+                    }
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+struct AddFoodView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddFoodView()
+    }
+}
---
+++ CoreDataTest/Views/EditFoodView.swift
@@ -0,0 +1,46 @@
+//
+//  EditFoodView.swift
+//  CoreDataTest
+//
+//  Created by мас on 03.06.2022.
+//
+
+import SwiftUI
+
+struct EditFoodView: View {
+    
+    @Environment (\.managedObjectContext) var managedObjectContext
+    @Environment(\.dismiss) var dismiss
+    
+    var food: FetchedResults<Food>.Element
+    
+    @State private var name = " "
+    @State private var calories: Double = 0
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("\(food.name!)", text: $name)
+                    .onAppear {
+                        name = food.name!
+                        calories = food.calories
+                    }
+                VStack {
+                    Text("Calories: \(Int(calories))")
+                    Slider(value: $calories, in: 0...2000, step: 10)
+                }
+                .padding()
+                
+                HStack {
+                    Spacer()
+                    Button("Submit") {
+                        DataController().editFood(food: food, name: name, calories: calories, context: managedObjectContext)
+                        dismiss()
+                    }
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
